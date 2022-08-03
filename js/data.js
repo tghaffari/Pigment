85d78b38a8f37e506133a2d5fe67be3c78098cb5
Tarra Ghaffari <tarrag2525@gmail.com>
2022-08-03 00:39:22 +0000

    Sent network request to retrieve data. Used that data to fill the color palette and gradient. Added code to store user inputs to local storage. For issue-1-user-can-randomly-generate-a-color-palette.

--- js/data.js
+++ js/data.js
@@ -3,5 +3,17 @@ var data = {
   view: 'new-project-form',
   entries: [],
   nextEntryId: 1,
-  currentPaletteColors: []
+  randomColorPalette: []
 };
+
+var dataJSON = localStorage.getItem('pigment-local-storage');
+if (dataJSON !== null) {
+  data = JSON.parse(dataJSON);
+}
+
+function saveEntryData(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('pigment-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', saveEntryData);
--- js/main.js
+++ js/main.js
@@ -1,15 +1,92 @@
+var $colorPalette = document.querySelector('.color-palette');
+var $paletteGradient = document.querySelector('.palette-gradient');
+var $rgbCode = document.querySelectorAll('.rgb-code');
+var $newPaletteButton = document.querySelector('.new-palette-button');
+var $projectEntryForm = document.querySelector('#new-project-form');
 
-function getColors() {
+$newPaletteButton.addEventListener('click', handleNewPaletteButtonClick);
+// Send a network request to generate random color palette
+
+function handleNewPaletteButtonClick(event) {
+  event.preventDefault();
+
+  data.randomColorPalette = {};
+  getRandomColors();
+}
+
+function getRandomColors() {
+  var targetUrl = encodeURIComponent('http://colormind.io/api/');
+  var body = {
+    model: 'default'
+  };
   var xhr = new XMLHttpRequest();
-  xhr.open('FETCH', 'http://www.colourlovers.com/api/palettes/random');
+  xhr.open('POST', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     // eslint-disable-next-line no-console
     console.log(xhr.status);
     // eslint-disable-next-line no-console
     console.log(xhr.response);
+    data.randomColorPalette = xhr.response.result;
+
+    setColorPalette(data.randomColorPalette);
+    setRgbCodes(data.randomColorPalette);
+    setGradient(data.randomColorPalette);
   });
-  xhr.send();
+  xhr.send(JSON.stringify(body));
 }
 
-getColors();
+getRandomColors();
+
+function generateCssStringRGB(red, green, blue, x, y) {
+  var rgbString = 'rgb(' + red + ',' + green + ',' + blue + ') ' + x + '% ' + y + '%';
+  return rgbString;
+}
+
+function setColorPalette(paletteColors) {
+  $colorPalette.style.background = 'linear-gradient(' +
+  0.25 + 'turn,' +
+  generateCssStringRGB(paletteColors[0][0], paletteColors[0][1], paletteColors[0][2], '0', '20') + ',' +
+  generateCssStringRGB(paletteColors[1][0], paletteColors[1][1], paletteColors[1][2], '0', '40') + ',' +
+  generateCssStringRGB(paletteColors[2][0], paletteColors[2][1], paletteColors[2][2], '0', '60') + ',' +
+  generateCssStringRGB(paletteColors[3][0], paletteColors[3][1], paletteColors[3][2], '0', '80') + ',' +
+  generateCssStringRGB(paletteColors[4][0], paletteColors[4][1], paletteColors[4][2], '0', '90') + ')';
+}
+
+function setRgbCodes(paletteColors) {
+  for (var i = 0; i < $rgbCode.length; i++) {
+    $rgbCode[i].textContent = '(' + paletteColors[i][0] + ', ' + paletteColors[i][1] + ', ' + paletteColors[i][2] + ')';
+  }
+}
+
+function setGradient(paletteColors) {
+  $paletteGradient.style.background = 'linear-gradient(' +
+  0.25 + 'turn,' +
+  generateCssStringRGB(paletteColors[0][0], paletteColors[0][1], paletteColors[0][2], '0', '15') + ',' +
+  generateCssStringRGB(paletteColors[1][0], paletteColors[1][1], paletteColors[1][2], '25', '35') + ',' +
+  generateCssStringRGB(paletteColors[2][0], paletteColors[2][1], paletteColors[2][2], '45', '55') + ',' +
+  generateCssStringRGB(paletteColors[3][0], paletteColors[3][1], paletteColors[3][2], '65', '75') + ',' +
+  generateCssStringRGB(paletteColors[4][0], paletteColors[4][1], paletteColors[4][2], '85', '90') + ')';
+}
+
+function saveProject(event) {
+  event.preventDefault();
+
+  var projectNameInput = $projectEntryForm.elements.projectName.value;
+  var projectDetailsInput = $projectEntryForm.elements.projectDetails.value;
+  var projectDeadlineInput = $projectEntryForm.elements.projectDeadline.value;
+  var newProjectEntry = {
+    projectName: projectNameInput,
+    projectDetails: projectDetailsInput,
+    projectDeadline: projectDeadlineInput,
+    colorPalette: data.randomColorPalette
+  };
+  newProjectEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(newProjectEntry);
+  data.randomColorPalette = {};
+
+  $projectEntryForm.reset();
+}
+
+$projectEntryForm.addEventListener('submit', saveProject);
